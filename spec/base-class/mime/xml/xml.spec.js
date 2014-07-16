1a43cb05eb22a3134a513480772aa8cca87690cd
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-16 01:18:36 -0700

    Add formatters

--- spec/base-class/mime/xml/xml.spec.js
+++ spec/base-class/mime/xml/xml.spec.js
@@ -1,17 +1,28 @@
 describe("Mime.XML", function() {
   describe("Adding Formats", function() {
-    it("parses xml", function() {
-      var xml = "<post> \
-                  <postId>1</postId> \
-                  <title>My Great Post</title> \
-                </post>";
+    var xml;
+    beforeEach(function() { 
+      xml = "<post> \
+              <postId>1</postId> \
+              <title>My Great Post</title> \
+            </post>";
 
+    });
+
+    it("parses xml", function() {
       expect(Mime.parse({type: "xml", data: xml})).toEqual({
         post : { 
           postId : '1', 
           title : 'My Great Post' 
         }
       });
+    });
+
+    it("formats xml", function() {
+      var javascriptObject = Mime.parse({type: "xml", data: xml});
+
+      expect(Mime.format({type: "xml", data: javascriptObject})).toEqual(
+        "<post><postId>1</postId><title>My Great Post</title></post>");
     });
   });
 });
