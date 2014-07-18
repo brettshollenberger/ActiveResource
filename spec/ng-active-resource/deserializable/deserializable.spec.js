cbeb91a4717f6cea231e6033d98a4203cfe011fb
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-17 21:47:11 -0700

    Refactor parse spec to deserialize spec

--- spec/ng-active-resource/deserializable/deserializable.spec.js
+++ spec/ng-active-resource/deserializable/deserializable.spec.js
@@ -1,4 +1,4 @@
-describe("Parsing", function() {
+describe("Deserializable", function() {
   describe("Adding model-specific parse methods", function() {
 
     var post, xml, response;
@@ -11,11 +11,6 @@ describe("Parsing", function() {
                       "<id>2</id>" + 
                     "</author>" +
                 "</post>";
-
-      backend.whenGET("https://api.edmodo.com/posts/1.json")
-        .respond(response);
-
-      spyOn($http, 'get').andCallThrough();
     });
 
     it("deserializes instances using parse method", function() {
@@ -49,12 +44,6 @@ describe("Parsing", function() {
       });
 
       expect(Post.deserialize(xml).author_id).toEqual(2);
-    });
-
-    it("passes to model-specific parse function after parsing the mimetype", function() {
-      Post.find(1).then(function(response) { post = response; });
-      backend.flush();
-      expect(post.author_id).toEqual(2);
     });
   });
 });
