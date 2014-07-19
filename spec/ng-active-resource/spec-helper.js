75c20b41e7083f41e40d896c763292be22cfc3e6
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-19 02:13:10 -0700

    Remove caching from creatable

--- spec/ng-active-resource/spec-helper.js
+++ spec/ng-active-resource/spec-helper.js
@@ -1,18 +1,20 @@
-var ngActiveResource, Mocks, Post, Comment, Person, Hat, Collection, TShirt, API, backend, $http, QueryCache;
+var ngActiveResource, Mocks, Post, Comment, Person, Hat, Collection, TShirt, API, backend, $http, $timeout, QueryCache;
 beforeEach(module('ngActiveResource'));
 beforeEach(module('Mocks'));
-beforeEach(inject(function(_ngActiveResource_, _Mocks_, _ARMime_, _ARAPI_, $httpBackend, _$http_, _ARQueryCache_) {
-  ngActiveResource = _ngActiveResource_;
-  Mocks            = _Mocks_;
-  Post             = Mocks.Post;
-  Comment          = Mocks.Comment;
-  Person           = Mocks.Person;
-  Hat              = Mocks.Hat;
-  Collection       = Mocks.Collection;
-  TShirt           = Mocks.TShirt;
-  Mime             = _ARMime_;
-  API              = _ARAPI_;
-  backend          = $httpBackend;
-  $http            = _$http_;
-  QueryCache       = _ARQueryCache_;
+beforeEach(inject(function(_ngActiveResource_, _Mocks_, _ARMime_, _ARAPI_, $httpBackend, _$http_, 
+  _ARQueryCache_, _$timeout_) {
+    ngActiveResource = _ngActiveResource_;
+    Mocks            = _Mocks_;
+    Post             = Mocks.Post;
+    Comment          = Mocks.Comment;
+    Person           = Mocks.Person;
+    Hat              = Mocks.Hat;
+    Collection       = Mocks.Collection;
+    TShirt           = Mocks.TShirt;
+    Mime             = _ARMime_;
+    API              = _ARAPI_;
+    backend          = $httpBackend;
+    $http            = _$http_;
+    $timeout         = _$timeout_;
+    QueryCache       = _ARQueryCache_;
 }));
