d573d852307387455aae6b60138c00340507d531
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-19 02:12:33 -0700

    Add Findable spec

---
+++ spec/ng-active-resource/findable/findable.spec.js
@@ -0,0 +1,19 @@
+describe("ARFindable", function() {
+  beforeEach(function() {
+    backend.whenGET("https://api.edmodo.com/posts/1.json").respond({id: 1, title: "My Great Post"});
+  });
+
+  it("finds cached instances if they exist already in the system", function() {
+    var post      = Post.new({id: 1});
+    var foundPost = Post.find(1);
+
+    expect(foundPost).toEqual(post);
+  });
+
+  it("queries the backend for instances otherwise", function() {
+    var post = Post.find(1);
+    backend.flush();
+    expect(post.title).toEqual("My Great Post");
+    expect(Post.cached[1]).toEqual(post);
+  });
+});
