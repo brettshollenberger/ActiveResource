e51aaa2916eb02d62448ef9ac2b4685f7d34e7a2
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-20 01:13:09 -0700

    Add Saveable

---
+++ spec/ng-active-resource/saveable/saveable.spec.js
@@ -0,0 +1,58 @@
+describe("ARSaveable", function() {
+  describe("Saving new instances", function() {
+    var post;
+    beforeEach(function() {
+      backend.whenPOST("https://api.edmodo.com/posts.json").respond({
+        id: 1,
+        title: "My Great Title",
+        content: "Wow, what a great post"
+      });
+
+      spyOn($http, "post").andCallThrough();
+
+      post = Post.new();
+      post.content = "Wow, what a great post";
+      post.$save({title: "My Great Title"});
+
+      backend.flush();
+    });
+
+    it("calls the createURL if the instance has no primary key", function() {
+      expect($http.post).toHaveBeenCalledWith("https://api.edmodo.com/posts.json");
+    });
+
+    it("updates the instance with results from the API", function() {
+      expect(post.id).toEqual(1);
+    });
+  });
+
+  describe("Saving instances with primary keys", function() {
+    var post;
+    beforeEach(function() {
+      backend.whenPUT("https://api.edmodo.com/posts/1.json").respond({
+        id: 1,
+        title: "Your Great Title",
+        content: "Wow, what a great post"
+      });
+
+      spyOn($http, "put").andCallThrough();
+
+      post = Post.new();
+      post.content = "Wow, what a great post";
+      post.$save({id: 1, title: "My Great Title"});
+
+      backend.flush();
+    });
+
+    it("calls the updateURL if the instance has a primary key", function() {
+      expect($http.put).toHaveBeenCalledWith("https://api.edmodo.com/posts/1.json");
+    });
+
+    it("updates the instance with results from the API", function() {
+      expect(post.title).toEqual("Your Great Title");
+    });
+  });
+
+  describe("Deserializing responses from the API", function() {
+  });
+});
