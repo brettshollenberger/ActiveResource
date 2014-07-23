8b393fc2ece85e51d6cf782baa29e41e411b7d5b
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-22 22:02:58 -0700

    Add Reflections spec

---
+++ spec/ng-active-resource/reflections/reflections.spec.js
@@ -0,0 +1,11 @@
+describe("ARReflection", function() {
+
+  var Farm;
+  beforeEach(function() {
+    Farm = function Farm() {}
+    Farm.include(Reflections);
+  });
+
+  iit("creates reflections", function() {
+  });
+});
