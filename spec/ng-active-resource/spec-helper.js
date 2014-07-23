d8e541d433ff2904dbe42b675bee8851697d4dc1
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-22 22:03:14 -0700

    Include Reflections in spec helper

--- spec/ng-active-resource/spec-helper.js
+++ spec/ng-active-resource/spec-helper.js
@@ -1,9 +1,9 @@
-var ngActiveResource, Mocks, Post, Comment, Person, Hat, Collection, TShirt, API, backend, $http, $timeout, QueryCache, Delegatable;
+var ngActiveResource, Mocks, Post, Comment, Person, Hat, Collection, TShirt, API, backend, $http, $timeout, QueryCache, Delegatable, mixin, Reflections;
 
 beforeEach(module('ngActiveResource'));
 beforeEach(module('Mocks'));
 beforeEach(inject(function(_ngActiveResource_, _Mocks_, _ARMime_, _ARAPI_, $httpBackend, _$http_, 
-  _ARQueryCache_, _$timeout_, _ARDelegatable_) {
+  _ARQueryCache_, _$timeout_, _ARDelegatable_, _ARMixin_, _ARReflections_) {
     ngActiveResource = _ngActiveResource_;
     Mocks            = _Mocks_;
     Post             = Mocks.Post;
@@ -19,6 +19,8 @@ beforeEach(inject(function(_ngActiveResource_, _Mocks_, _ARMime_, _ARAPI_, $http
     $timeout         = _$timeout_;
     QueryCache       = _ARQueryCache_;
     Delegatable      = _ARDelegatable_;
+    mixin            = _ARMixin_;
+    Reflections      = _ARReflections_;
 
     API.configure(function(config) {
       config.baseURL = "https://api.edmodo.com";
