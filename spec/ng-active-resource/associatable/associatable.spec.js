c8027672e64facaf40c586cd754dc6efbb9e6ad3
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-07-24 20:55:56 -0700

    Remove old association code

--- spec/ng-active-resource/associatable/associatable.spec.js
+++ spec/ng-active-resource/associatable/associatable.spec.js
@@ -1,89 +1,29 @@
-describe('ARAssociatable', function() {
-  describe("class#hasMany", function() {
-    it("adds hasMany association to class", function() {
-      expect(_.keys(Post.associations)).toContain("comments");
+var comment, comment2, post;
+
+ddescribe("ARAssociatable", function() {
+  describe("HasMany", function() {
+    it("initializes hasMany associations", function() {
+      comment = Comment.new({id: 1});
+      post    = Post.new({id: 1, comments: comment});
+
+      expect(post.comments).toContain(comment);
     });
 
-    it("intuits name of associated class by default", function() {
-      expect(Post.associations.comments.klass).toEqual(Comment);
+    it("updates hasMany associations", function() {
+      comment  = Comment.new({id: 1});
+      comment2 = Comment.new({id: 2});
+      post     = Post.new({id: 1, comments: comment});
+      post.update({comments: comment2});
+
+      expect(post.comments).not.toContain(comment);
+      expect(post.comments).toContain(comment2);
     });
 
-    it("finds associated provider by named argument", function() {
-      expect(Person.associations.hats.klass).toEqual(Hat);
-    });
+    it("has methods to add associated instances", function() {
+      post    = Post.new({id: 1});
+      comment = post.comments.new({id: 2});
 
-    describe("Creating instances", function() {
-      var post;
-      beforeEach(function() {
-         post = Post.new();
-      });
-
-      it("creates empty hasMany association", function() {
-        expect(post.comments).toEqual([]);
-      });
-
-      it("adds associations with new", function() {
-        var comment = post.comments.new({});
-        expect(post.comments).toContain(comment);
-      });
-
-      it("associates itself with new instances", function() {
-        var comment = post.comments.new({});
-        expect(comment.post).toEqual(post);
-      });
-
-      it("adds new instances on create", function() {
-        var comment  = Comment.new();
-        var comment2 = Comment.new();
-        var post     = Post.new({comments: [comment, comment2]});
-
-        expect(post.comments).toContain(comment);
-        expect(post.comments).toContain(comment2);
-        expect(comment.post).toEqual(post);
-        expect(comment2.post).toEqual(post);
-      });
-    });
-  });
-
-  describe("class#belongsTo", function() {
-    it("adds belongsTo association to class", function() {
-      expect(_.keys(Comment.associations)).toContain("post");
-    });
-
-    it("overrides provider name if provided", function() {
-      expect(Hat.associations.collection.klass).toEqual(Collection);
-    });
-
-    it("adds foreign key to association", function() {
-      expect(Comment.associations.post.foreignKey).toContain("post_id");
-    });
-
-    it("overrides foreign key if provided", function() {
-      expect(Hat.associations.person.foreignKey).toContain("user_id");
-    });
-
-    describe("Creating instances", function() {
-      var post, comment;
-      beforeEach(function() {
-         post    = Post.new();
-         comment = Comment.new({post: post});
-      });
-
-      it("creates belongsTo association", function() {
-        expect(comment.post).toEqual(post);
-      });
-
-      it("updates belongsTo association when known", function() {
-        comment = Comment.new();
-        comment.update({post: post});
-
-        expect(comment.post).toEqual(post);
-      });
-
-      it("creates no association by default", function() {
-        comment = Comment.new({});
-        expect(comment.post).toBeUndefined();
-      });
+      expect(post.comments).toContain(comment);
     });
   });
 });
