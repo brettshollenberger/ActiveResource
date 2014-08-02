3759941e05446171ebab22e2f6639910154caa32
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-08-02 01:53:07 -0700

    Change $httpConfig#data to the idiomatic #params

--- spec/ng-active-resource/http/config/config.spec.js
+++ spec/ng-active-resource/http/config/config.spec.js
@@ -1,40 +1,46 @@
 describe("ARHTTPConfig", function() {
   beforeEach(function() {
     API.configure(function(config) {
-      config.data = {
+      config.params = {
         api_token: "1234"
       }
     });
 
-    backend.whenGET("https://api.edmodo.com/posts/1.json")
+    backend.whenGET("https://api.edmodo.com/posts/1.json?api_token=1234")
       .respond(200, {id: 1, title: "My Great Post", author_id: 1}, {});
 
-    backend.whenPOST("https://api.edmodo.com/posts.json")
+    backend.whenGET("https://api.edmodo.com/posts/1.json?api_token=1234&published=true")
+      .respond(200, {id: 1, title: "My Great Post", author_id: 1}, {});
+
+    backend.whenGET("https://api.edmodo.com/posts/1.json?api_token=5678")
+      .respond(200, {id: 1, title: "My Great Post", author_id: 1}, {});
+
+    backend.whenPOST("https://api.edmodo.com/posts.json?api_token=1234")
       .respond(200, {id: 1, title: "My Great Post", author_id: 1}, {});
 
     spyOn($http, "get").andCallThrough();
     spyOn($http, "post").andCallThrough();
   });
 
-  it("adds default data to requests", function() {
+  it("adds default params to requests", function() {
     var post = Post.find(1);
 
     backend.flush();
 
-    expect($http.get.mostRecentCall.args[1].data).toEqual({api_token: "1234"});
+    expect($http.get.mostRecentCall.args[1].params).toEqual({api_token: "1234"});
   });
 
-  it("adds additional data to calls", function() {
-    var post = Post.find(1, {data: {published: true}});
+  it("adds additional params to calls", function() {
+    var post = Post.find(1, {params: {published: true}});
 
     backend.flush();
 
-    expect($http.get.mostRecentCall.args[1].data).toEqual({published: true, api_token: "1234"});
+    expect($http.get.mostRecentCall.args[1].params).toEqual({published: true, api_token: "1234"});
   });
 
   it("overrides defaults on individual APIs", function() {
     Post.api.configure(function(config) {
-      config.data = {
+      config.params = {
         api_token: "5678"
       }
     });
@@ -43,14 +49,14 @@ describe("ARHTTPConfig", function() {
 
     backend.flush();
 
-    expect($http.get.mostRecentCall.args[1].data).toEqual({api_token: "5678"});
+    expect($http.get.mostRecentCall.args[1].params).toEqual({api_token: "5678"});
   });
 
-  xit("uses data on all actions", function() {
+  it("uses params on all actions", function() {
     var post = Post.$create({title: "My Great Title"});
 
     backend.flush();
 
-    expect($http.post.mostRecentCall.object()).toEqual({api_token: "1234"});
+    expect($http.post.mostRecentCall.args[2].params).toEqual({api_token: "1234"});
   });
 });
