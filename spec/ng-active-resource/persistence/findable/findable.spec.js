cc7947ad0bfc63b9ff38725f1a0a13957e8fa197
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-08-02 21:52:35 -0700

    Findable deserializes backend responses

--- spec/ng-active-resource/persistence/findable/findable.spec.js
+++ spec/ng-active-resource/persistence/findable/findable.spec.js
@@ -1,7 +1,10 @@
 describe("ARFindable", function() {
   beforeEach(function() {
-    backend.whenGET("https://api.edmodo.com/posts/1.json").respond({id: 1, title: "My Great Post"});
+    backend.whenGET("https://api.edmodo.com/posts/1.json")
+      .respond({id: 1, title: "My Great Post"});
 
+    backend.whenGET("https://api.edmodo.com/posts/1.xml")
+      .respond(200, "<post><id>1</id><title>My Great Post</title></post>");
   });
 
   it("finds cached instances if they exist already in the system", function() {
@@ -23,6 +26,20 @@ describe("ARFindable", function() {
     backend.flush();
     expect(post.title).toEqual("My Great Post");
     expect(Post.cached[1]).toEqual(post);
+  });
+
+  it("deserializes backend responses", function() {
+    Post.api.configure(function(config) {
+      config.format            = "xml";
+      config.unwrapRootElement = true;
+    });
+
+    var post = Post.find(1);
+
+    backend.flush();
+
+    expect(post.id).toEqual(1);
+    expect(post.title).toEqual("My Great Post");
   });
 
   it("takes a promise and returns the instance", function() {
