1259f5747bd05b858e8838604a0aa1a00b000026
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-08-03 01:00:10 -0700

    Add additional dirty logic

--- spec/ng-active-resource/dirty/dirty.spec.js
+++ spec/ng-active-resource/dirty/dirty.spec.js
@@ -2,6 +2,9 @@ describe("ARDirty", function() {
   var post;
   beforeEach(function() {
     post = Post.new({id: 1});
+
+    backend.whenPUT("https://api.edmodo.com/posts/1.json")
+           .respond(200, {id: 1});
   });
 
   it("lists changed attributes", function() {
@@ -13,9 +16,6 @@ describe("ARDirty", function() {
   });
 
   it("is not dirty if attributes have not changed since last save", function() {
-    backend.whenPUT("https://api.edmodo.com/posts/1.json")
-           .respond(200, {id: 1});
-
     post.$save();
 
     backend.flush();
@@ -33,6 +33,81 @@ describe("ARDirty", function() {
     backend.flush();
 
     expect(post.changedAttributes()).toEqual([]);
+    expect(post.dirty()).toEqual(false);
+  });
+
+  it("is not dirty if attributes are fundamentally unchanged", function() {
+    post.$save();
+    backend.flush();
+
+    post.title = "";
+
+    expect(post.dirty()).toBe(false);
+
+    post.title = "Cool Post";
+    post.$save();
+    backend.flush();
+
+    post.title = "";
+
+    expect(post.changedAttributes()).toEqual(["title"]);
+    expect(post.dirty()).toBe(true);
+  });
+
+  it("is not dirty if numbers and strings are compared", function() {
+    post.$save();
+    backend.flush();
+
+    post.id = "1";
+    expect(post.dirty()).toBe(false);
+  });
+
+  it("compares boolean values", function() {
+    post.published = true;
+    post.$save();
+    backend.flush();
+
+    post.published = "true";
+    expect(post.dirty()).toBe(false);
+
+    post.published = false;
+    expect(post.dirty()).toBe(true);
+  });
+
+  it("compares objects", function() {
+    post.meta = {
+      format: "html"
+    }
+
+    post.$save();
+    backend.flush();
+
+    post.meta = {
+      format: "xml"
+    }
+
+    expect(post.dirty()).toEqual(true);
+
+    post.meta = "";
+
+    expect(post.dirty()).toEqual(true);
+
+    post.meta = {
+      format: "html"
+    }
+
+    expect(post.dirty()).toEqual(false);
+  });
+
+  it("compares arrays", function() {
+    post.tags = ["one", "two", "three", "schfoor"];
+    post.$save();
+    backend.flush();
+
+    post.tags = ["fun", "two"];
+    expect(post.dirty()).toEqual(true);
+
+    post.tags = ["one", "two", "three", "schfoor"];
     expect(post.dirty()).toEqual(false);
   });
 });
