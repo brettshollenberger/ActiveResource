b1ac2b9e577df664179f59edd466769030ba8dac
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-08-05 21:15:27 -0700

    Add QueryCache to Refinable

--- spec/ng-active-resource/persistence/refinable/refinable.spec.js
+++ spec/ng-active-resource/persistence/refinable/refinable.spec.js
@@ -4,9 +4,9 @@ ddescribe("ARRefinable", function() {
       .respond(200, [{id: 1, title: "My Great Post", author_id: 1},
                 {id: 2, title: "Joan of Shark", author_id: 1}], {});
 
-    backend.whenGET("https://api.edmodo.com/posts.json")
-      .respond(200, [{id: 1, title: "My Great Post", author_id: 1},
-                {id: 2, title: "Joan of Shark", author_id: 1}], {});
+    backend.whenGET("https://api.edmodo.com/posts.json?author_id=1&page=2")
+      .respond(200, [{id: 3, title: "A very new post", author_id: 1},
+                {id: 4, title: "My favorite thangs", author_id: 1}], {});
   });
 
   it("creates an empty query set", function() {
@@ -19,12 +19,40 @@ ddescribe("ARRefinable", function() {
     expect(posts.klass).toEqual(Post);
   });
 
-  iit("adds initial instances via #where method", function() {
+  it("adds initial instances via #where method", function() {
     var posts = new ngActiveResource.Refinable(Post);
     posts.where({author_id: 1});
     backend.flush();
 
     expect(posts.first().id).toEqual(1);
+  });
+
+  it("stashes queries", function() {
+    var posts = new ngActiveResource.Refinable(Post);
+    posts.where({author_id: 1});
+    backend.flush();
+
+    expect(posts.queries.find({author_id: 1}).first()).toEqual(posts.first());
+    expect(posts.queries.find({author_id: 1}).last()).toEqual(posts.last());
+  });
+
+  iit("stashes the most recent call", function() {
+    var posts = new ngActiveResource.Refinable(Post);
+    posts.where({author_id: 1});
+    backend.flush();
+
+    expect(posts.mostRecentCall).toEqual({author_id: 1});
+  });
+
+  it("refines queries", function() {
+    var posts = new ngActiveResource.Refinable(Post);
+    posts.where({author_id: 1});
+    backend.flush();
+    posts.where({page: 2});
+    backend.flush();
+
+    expect(posts.first().id).toEqual(3);
+    expect(posts.last().id).toEqual(4);
   });
 
   it("transforms the json into model instances", function() {
