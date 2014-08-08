ada4ef42edfbe09ef26cdb1aa792e25faa4f1691
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-08-07 21:44:57 -0700

    Add test for adding events to instances

--- spec/ng-active-resource/eventable/eventable.spec.js
+++ spec/ng-active-resource/eventable/eventable.spec.js
@@ -1,26 +1,49 @@
 describe("AREventable", function() {
-  var person, instance, attributes;
-  beforeEach(function() {
-    person = Person.new({
-      name: "Troy Barnes",
-      age: 25,
-      email: "troy@greendale.edu"
+  describe("Evented Models", function() {
+    var person, instance, attributes;
+    beforeEach(function() {
+      person = Person.new({
+        name: "Troy Barnes",
+             age: 25,
+             email: "troy@greendale.edu"
+      });
+
+      Person.before("save", function(i, a) {
+        instance   = i;
+        attributes = a;
+      });
+
+      backend.whenPOST("https://api.edmodo.com/people.json")
+      .respond(200, {id: 1, name: "Troy Barnes", age: 25, email: "troy@greendale.edu"});
     });
 
-    Person.before("save", function(i, a) {
-      instance   = i;
-      attributes = a;
-    });
+    it("passes the instance and attributes on save", function() {
+      person.$save({fun: true});
+      backend.flush();
 
-    backend.whenPOST("https://api.edmodo.com/people.json")
-           .respond(200, {id: 1, name: "Troy Barnes", age: 25, email: "troy@greendale.edu"});
+      expect(instance).toEqual(person);
+      expect(attributes).toEqual({fun: true});
+    });
   });
 
-  it("passes the instance and attributes on save", function() {
-    person.$save({fun: true});
-    backend.flush();
+  describe("Evented Instances", function() {
+    var post, posts;
+    beforeEach(function() {
+      backend.whenGET("https://api.edmodo.com/posts.json?author_id=1&page=1")
+             .respond(200, [{id: 2, author_id: 1}]);
 
-    expect(instance).toEqual(person);
-    expect(attributes).toEqual({fun: true});
+      posts = Post.where({author_id: 1});
+      backend.flush();
+
+      post = Post.new({});
+
+      post.after("save", function() {
+        posts.push(post);
+      });
+    });
+
+    iit("features an interface for adding events to instances", function() {
+      expect(posts).toInclude(post);
+    });
   });
 });
