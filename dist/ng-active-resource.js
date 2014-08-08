cc06c3d813e4eb8a8903624a118a90b4d80cc046
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-08-08 02:52:12 -0700

    Release v0.7.12

--- dist/ng-active-resource.js
+++ dist/ng-active-resource.js
@@ -461,6 +461,7 @@ angular.module('ngActiveResource').factory('ARBase', [
       this.include(ComputedProperty);
       this.include(Functional);
       this.include(Promiseable);
+      this.include(Eventable);
     }
     return Base;
   }
@@ -502,62 +503,81 @@ angular.module('ngActiveResource').factory('ARCacheable', [
     return Cache;
   }
 ]);
+// ComputedProperty(name, valueFn, dependents)
+//
+// @param name       {string}         - The name of the property to be computed from other properties
+//
+// @param valueFn    {func}           - The function used to compute the new property from the others
+//
+// @param dependents {string | array} - The name of the property or list of the properties that this 
+//                                      property depends upon.
+//
+// Example:
+//
+//    function Tshirt(attributes) {
+//      this.number('price');
+//
+//      this.computedProperty('salePrice', function() {
+//        return this.price - (this.price * 0.2);
+//      }, 'price');
+//
+//      this.computedProperty('superSalePrice', function() {
+//        return this.price - this.salePrice;
+//      }, ['price', 'salePrice']);
+//    }
+//
+// The computed property function creates dynamically evaluated relationships between properties. In the example above,
+// a change to the price of an item updates its salePrice automatically, and an update to either price or salePrice automatically
+// updates the superSalePrice.
+//
+// Computed property actually allows you to create inter-dependent relationships:
+//
+//    function Order() {
+//      this.computedProperty('price', function() {
+//        return this.salePrice + 5;
+//      }, 'salePrice');
+//
+//      this.computedProperty('salePrice', function() {
+//        return this.price - 5;
+//      }, 'price');
+//    }
+//
+// In the example above, an update to price updates the salePrice, and an update to salePrice updates the price. Even with entire
+// models of complex inter-dependencies, these computed properties will update one another without causing an infinite cycle.
+//
+// An example use-case is a spreadsheet application, where updating one field should cause related fields to update, and likewise
+// updating the related field should cause the same update.
 angular.module('ngActiveResource').factory('ARComputedProperty', [function () {
     function ComputedProperty() {
-      // Instance#computedProperty(name, valueFn, dependents)
-      //
-      // @param name       {string}         - The name of the property to be computed from other properties
-      //
-      // @param valueFn    {func}           - The function used to compute the new property from the others
-      //
-      // @param dependents {string | array} - The name of the property or list of the properties that this 
-      //                                      property depends upon.
-      //
-      // Example:
-      //
-      //    function Tshirt(attributes) {
-      //      this.number('price');
-      //
-      //      this.computedProperty('salePrice', function() {
-      //        return this.price - (this.price * 0.2);
-      //      }, 'price');
-      //
-      //      this.computedProperty('superSalePrice', function() {
-      //        return this.price - this.salePrice;
-      //      }, ['price', 'salePrice']);
-      //    }
-      //
-      // The computed property function creates configurable getters and setters (that can thus be reconfigured).
-      // In the first example, the price setter calls the salePrice setter whenever it updates. In the second
-      // example, the salePrice setter continues to be called by the price setter, and additionally calls the
-      // superSalePrice setter afterward.
-      //
-      // This chainability allows us to create complex inter-dependencies, where an update to one property
-      // updates many others. In order to all this to occur, we use the `__lookupSetter__` function to retrieve
-      // the value of the previous setter.
       this.__computedProperty = function (name, valueFn, dependents) {
+        if (!this.computedProperties) {
+          privateVariable(this, 'computedProperties', []);
+        }
+        this.computedProperties.push([
+          name,
+          valueFn,
+          dependents
+        ]);
+      };
+      this.__buildComputedProperties = function () {
+        _.each(this.computedProperties, function (computedProperty) {
+          buildComputedProperty.apply(this, computedProperty);
+        }, this);
+        delete this.__buildComputedProperties;
+        delete this.computedProperties;
+      };
+      function buildComputedProperty(name, valueFn, dependents) {
         var instance = this;
         var data = this.constructor.constructing.attributes;
-        if (!dependents)
+        instance.justSet = [];
+        if (!dependents) {
           dependents = [];
-        if (!dependents.push)
+        }
+        if (!dependents.push) {
           dependents = [dependents];
-        var local2;
-        Object.defineProperty(instance, name, {
-          enumerable: true,
-          configurable: true,
-          get: function () {
-            return local2;
-          },
-          set: function () {
-            local2 = valueFn.apply(instance);
-            return local2;
-          }
-        });
+        }
         _.each(dependents, function (dependent) {
-          var local;
-          var previousSetter = instance.__lookupSetter__(dependent);
-          var dependentVal = instance[dependent];
+          var local, originalVal = instance[dependent], previousSetter = instance.__lookupSetter__(dependent);
           Object.defineProperty(instance, dependent, {
             enumerable: true,
             configurable: true,
@@ -565,24 +585,23 @@ angular.module('ngActiveResource').factory('ARComputedProperty', [function () {
               return local;
             },
             set: function (val) {
-              if (val !== undefined && val != 'set')
-                local = val;
-              if (previousSetter) {
-                if (local == val)
-                  previousSetter();
-                else
-                  local = previousSetter();
-              }
-              instance[name] = 'set';
+              local = val;
+              instance.emit(dependent + ':set');
               return local;
             }
           });
-          if (data && data[dependent])
-            instance[dependent] = data[dependent];
-          else
-            instance[dependent] = dependentVal;
+          // Allow inter-dependencies between properties without creating
+          // an infinite loop between them.
+          instance.on(dependent + ':set', function () {
+            instance.justSet.push(dependent);
+            if (!_.include(instance.justSet, name)) {
+              instance[name] = valueFn.apply(instance);
+            }
+            instance.justSet = [];
+          });
+          instance[dependent] = originalVal;
         });
-      };
+      }
     }
     return ComputedProperty;
   }]);
@@ -643,7 +662,9 @@ angular.module('ngActiveResource').factory('ARConfigurable', [function () {
     }
     return Configurable;
   }]);
-angular.module('ngActiveResource').factory('ARDefinable', [function () {
+angular.module('ngActiveResource').factory('ARDefinable', [
+  'AREventable',
+  function (Eventable) {
     Definable.included = function (klass) {
       klass.before('new', function (attributes) {
         klass.constructing = { attributes: attributes };
@@ -670,7 +691,8 @@ angular.module('ngActiveResource').factory('ARDefinable', [function () {
       }
     }
     return Definable;
-  }]);
+  }
+]);
 angular.module('ngActiveResource').factory('ARDelegatable', [function () {
     Delegatable.extended = function (klass) {
       klass.delegate = new Delegatable().__delegate;
@@ -918,6 +940,7 @@ angular.module('ngActiveResource').factory('AREventable', [
       var events = { handlers: {} };
       Object.defineProperty(this, 'emit', {
         enumerable: true,
+        configurable: true,
         value: function (eventType) {
           if (!events.handlers[eventType])
             return;
@@ -2107,8 +2130,9 @@ angular.module('ngActiveResource').factory('ARCreatable', [
         attributes = attributes || {};
         this.emit('new:called', attributes);
         var instance = new this(attributes);
-        this.emit('new:beginning', instance, attributes);
         instance.extend(Eventable, { private: true });
+        instance.buildComputedProperties();
+        this.emit('new:beginning', instance, attributes);
         this.emit('new:complete', instance);
         return instance;
       };
