3eb727def1440b89272a71e6e756d57b49c3c55e
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-08-15 00:16:56 -0700

    Add append query string

--- spec/ng-active-resource/spec-helper.js
+++ spec/ng-active-resource/spec-helper.js
@@ -1,10 +1,10 @@
-var ngActiveResource, Mocks, Player, Ship, Post, Author, Comment, Person, Hat, Collection, TShirt, Member, District, School, API, backend, $http, $timeout, Delegatable, mixin, Reflections, foreignkeyify, dropHasMany, serializeAssociations, params;
+var ngActiveResource, Mocks, Player, Ship, Post, Author, Comment, Person, Hat, Collection, TShirt, Member, District, School, API, backend, $http, $timeout, Delegatable, mixin, Reflections, foreignkeyify, dropHasMany, serializeAssociations, params, $location;
 
 beforeEach(module('ngActiveResource'));
 beforeEach(module('Mocks'));
 beforeEach(inject(function(_ngActiveResource_, _Mocks_, _ARMime_, _ARAPI_, $httpBackend, _$http_, 
   _$timeout_, _ARDelegatable_, _ARMixin_, _ARReflections_, _Foreignkeyify_, _DropHasMany_,
-  _ARSerializeAssociations_, _ARParams_) {
+  _ARSerializeAssociations_, _ARParams_, _$location_) {
     ngActiveResource      = _ngActiveResource_;
     Mocks                 = _Mocks_;
     Player                = Mocks.Player;
@@ -31,6 +31,7 @@ beforeEach(inject(function(_ngActiveResource_, _Mocks_, _ARMime_, _ARAPI_, $http
     dropHasMany           = _DropHasMany_;
     serializeAssociations = _ARSerializeAssociations_;
     params                = _ARParams_;
+    $location             = _$location_;
 
     API.configure(function(config) {
       config.baseURL = "https://api.edmodo.com";
